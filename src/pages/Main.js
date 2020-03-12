44bcc4ae3357ff71017e900cdf77771e0f4d0653
Alec Diaz <alecdiaz1@yahoo.com>
2020-03-11 20:24:40 -0700

    finish modal

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,36 +2,54 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Calendar from 'react-calendar';
 
-import { Typography, Divider, Button, Card } from 'antd';
+import { Button, Card, Modal, Input } from 'antd';
 
 import { Row, Col } from 'antd';
 import '../Calendar.css';
 
 import PageLayout from "../components/Layout";
-import LandingImg from "../landingpic.png"
+import RatingImg from "../img/rating.png"
 
 export default function Home() {
     const [date, setDate] = useState(new Date());
+    const [visible, setVisible] = useState(false);
 
     const { Meta } = Card;
+    const { TextArea } = Input;
 
-    const logDay = () => {
-        console.log('clicked')
+    const handleOpenModal = () => {
+        setVisible(true)
+    };
+
+    const handleCloseModal = () => {
+        setVisible(false)
     };
 
     return (
         <PageLayout>
+            <Modal
+                // title="How do you feel today?"
+                visible={visible}
+                onOk={handleCloseModal}
+                onCancel={handleCloseModal}
+            >
+                <h2 style={{color: '#474747'}}>How do you feel today?</h2>
+                <img src={RatingImg}/>
+                <h4 style={{color: '#474747', marginTop: '4em'}}>Journal</h4>
+                <TextArea rows={4}/>
+            </Modal>
+
             <Row style={{marginTop: '8em'}}>
                 <Col span={12} offset={6} style={{color: '#000000'}}>
                     <Calendar
                         calendarType={"US"}
-                        onClickDay={logDay}/>
+                        onClickDay={handleOpenModal}/>
                 </Col>
             </Row>
             <Row justify="center">
                 <Col span={4} offset={10}>
                     <Button
-                        onClick={logDay}
+                        onClick={handleOpenModal}
                         style={{marginTop: '2em'}}
                         block
                     >+ How I felt today</Button>
